f4cef481d2cdb99b074d0d6afb4199d0df37c471
Devin Spikowski <vegeta897@gmail.com>
2020-04-18 10:13:23 -0400

    Remove react-pixi, add custom WebGL detection

--- packages/d-zone-webapp/src/App.js
+++ packages/d-zone-webapp/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { Stage, useApp } from '@inlet/react-pixi';
-import ReactViewport from './ReactViewport';
 import Engine from './Engine';
 
-const ReactPixiViewport = React.forwardRef(({ children }, ref) => {
-    const app = useApp();
-    return <ReactViewport app={app} ref={ref}>{children}</ReactViewport>;
-});
-
 function App() {
-    const refViewport = React.useCallback(node => {
-        if(node !== null) {
-            Engine(node);
-        }
-    }, []);
+
+    useEffect(() => {
+        Engine();
+    });
 
     return (
         <div className='App'>
-            <Stage width={500} height={500} options={{ autoResize: true, sharedTicker: true }}>
-                <ReactPixiViewport ref={refViewport} />
-            </Stage>
+
         </div>
     );
 }
--- packages/d-zone-webapp/src/Engine/index.js
+++ packages/d-zone-webapp/src/Engine/index.js
@@ -1,4 +1,50 @@
-import { Graphics } from 'pixi.js';
+import * as PIXI from 'pixi.js';
+import { Viewport } from 'pixi-viewport';
+
+// Override renderer create function to use custom WebGL test
+PIXI.Renderer.create = function create (options) {
+    if (isWebGLSupported()) {
+        return new PIXI.Renderer(options);
+    }
+    throw new Error('WebGL unsupported in this browser, use "pixi.js-legacy" for fallback canvas2d support.');
+};
+
+// WebGL support test function from Three.js
+function isWebGLSupported () {
+    try {
+        let canvas = document.createElement('canvas');
+        return !!window.WebGLRenderingContext &&
+            (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'));
+    } catch(e) {
+        return false;
+    }
+}
+
+const app = new PIXI.Application({
+    width: 1280,
+    height: 720
+});
+document.body.appendChild(app.view);
+
+const viewport = new Viewport({
+    screenWidth: 1280,
+    screenHeight: 720,
+    worldWidth: 1000,
+    worldHeight: 1000,
+    interaction: app.renderer.plugins.interaction,
+    disableOnContextMenu: true
+});
+
+app.stage.addChild(viewport);
+
+viewport.on('drag-start', () => console.log('drag-start'));
+viewport.on('drag-end', () => console.log('drag-end'));
+
+viewport
+    .drag()
+    .pinch()
+    .wheel()
+    .decelerate();
 
 const now = global.performance && global.performance.now ? function() {
     return performance.now()
@@ -6,12 +52,13 @@ const now = global.performance && global.performance.now ? function() {
     return +new Date()
 };
 
-function Engine(viewport) {
-    let testBox = new Graphics()
-        .beginFill(0x0099ee)
-        .drawRect(0, 0, 100, 100);
+let testBox = new PIXI.Graphics()
+    .beginFill(0x0099ee)
+    .drawRect(0, 0, 100, 100);
 
-    console.log(viewport);
+function Engine() {
+
+    console.log('engine started');
 
     viewport.addChild(testBox);
 
