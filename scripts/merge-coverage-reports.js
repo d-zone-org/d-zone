5fbe38613a059b606d8319466426f34c2b02fd25
Sagnik Pradhan <markten6321@gmail.com>
2020-06-29 08:08:44 +0530

    Removed hardcoded coverage file paths

--- scripts/merge-coverage-reports.js
+++ scripts/merge-coverage-reports.js
@@ -1,12 +1,27 @@
 const { createCoverageMap } = require('istanbul-lib-coverage')
 const { createReporter } = require('istanbul-api')
+const { workspaces } = require('../package.json')
 const path = require('path')
+const fs = require('fs')
 
-const lcovFilePaths = [
-	'../packages/server/coverage/coverage-final.json',
-	'../packages/webapp/coverage/coverage-final.json',
-].map((fileRelativePath) => path.join(__dirname, fileRelativePath))
+// We get the workspace roots from package.json
+// Then get their absolute urls
+// Then filter the ones that exist
+const lcovFilePaths = workspaces
+	.map((filePathRelativeToRoot) =>
+		path.join(
+			__dirname,
+			'..',
+			filePathRelativeToRoot,
+			'/coverage/coverage-final.json'
+		)
+	)
+	.filter((filePath) => fs.existsSync(filePath))
 
+// Log the files found
+console.log(`Found lcov files: \n${lcovFilePaths.join('\n')}`)
+
+// Merge and write the coverage
 const coverageMap = createCoverageMap({})
 const reporter = createReporter()
 reporter.add('lcovonly')
