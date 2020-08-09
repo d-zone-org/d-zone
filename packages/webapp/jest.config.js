4b2ab5a3f9f69f8699ce6792a32ae5dc86daaad3
Devin Spikowski <vegeta897@gmail.com>
2020-08-09 00:42:24 -0400

    Add Jest

---
+++ packages/webapp/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	clearMocks: true,
+
+	coverageDirectory: '../coverage',
+
+	coverageReporters: ['json', 'text'],
+
+	moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+
+	rootDir: '__tests__',
+
+	setupFiles: [
+		/*'jest-canvas-mock'*/
+	],
+}
