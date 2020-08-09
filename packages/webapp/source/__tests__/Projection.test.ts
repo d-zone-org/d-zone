0c7d4a4cc1214dce8adb0f9ba9717856f8e181c6
Devin Spikowski <vegeta897@gmail.com>
2020-08-09 07:34:35 -0400

    Fix import path

--- packages/webapp/source/__tests__/Projection.test.ts
+++ packages/webapp/source/__tests__/Projection.test.ts
@@ -1,4 +1,4 @@
-import { get2dCoordsFromIso, getZIndex, SCALE } from '../Game/Common/Projection'
+import { get2dCoordsFromIso, getZIndex, SCALE } from '../Components/Game/Common/Projection'
 
 describe(get2dCoordsFromIso, () => {
 	test('returns x and y values', () => {
