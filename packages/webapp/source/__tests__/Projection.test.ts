8ae5b34319fb4f1329faf540d77e1c709548e150
Devin Spikowski <vegeta897@gmail.com>
2020-08-09 05:14:23 -0400

    Improve tests

--- packages/webapp/source/__tests__/Projection.test.ts
+++ packages/webapp/source/__tests__/Projection.test.ts
@@ -1,18 +1,16 @@
-import { get2dCoordsFromIso, getZIndex } from '../Game/Common/Projection'
+import { get2dCoordsFromIso, getZIndex, SCALE } from '../Game/Common/Projection'
 
 describe(get2dCoordsFromIso, () => {
 	test('returns x and y values', () => {
-		let coords = get2dCoordsFromIso(0, 0, 0)
-		expect(coords.length).toEqual(2)
-		expect(coords[0]).toEqual(0)
+		let coords = get2dCoordsFromIso(1, 2, 3)
+		expect(coords[0]).toEqual(-2 * SCALE)
 		expect(coords[1]).toEqual(0)
 	})
 })
 
 describe(getZIndex, () => {
-	test('returns number', () => {
-		let zIndex = getZIndex(0, 0, 0)
-		expect(zIndex).toEqual(0)
-		expect(zIndex).not.toBeNaN()
+	test('returns z-index', () => {
+		let zIndex = getZIndex(1, 2, 3)
+		expect(zIndex).toEqual(3.03)
 	})
 })
