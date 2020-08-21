2292de23325f0791fcdab7a4a090c3332d0ba50d
Devin Spikowski <vegeta897@gmail.com>
2020-08-21 19:35:40 -0400

    Fix getComponent methods corresponding with ECSY fix

--- packages/webapp/source/components/Game/Engine/systems/HopSystem.ts
+++ packages/webapp/source/components/Game/Engine/systems/HopSystem.ts
@@ -15,14 +15,14 @@ export default class HopSystem extends System {
 		let hopping = this.queries.hopping.results
 		for (let i = hopping.length - 1; i >= 0; i--) {
 			let entity = hopping[i]
-			let hop = entity.getMutableComponent!(Hop)
+			let hop = entity.getMutableComponent(Hop)!
 			let frame = Math.floor(hopFrameCount * hop.progress)
 			if (hop.progress >= 1) {
-				let transform = entity.getMutableComponent!(Transform)
+				let transform = entity.getMutableComponent(Transform)!
 				transform.x += hop.x
 				transform.y += hop.y
 				transform.z += hop.z
-				let sprite = entity.getMutableComponent!(Sprite)
+				let sprite = entity.getMutableComponent(Sprite)!
 				if (hop.direction === 'east') sprite.texture = 'cube:0'
 				else if (hop.direction === 'south') sprite.texture = 'cube:1'
 				else sprite.texture = 'cube:2'
@@ -30,7 +30,7 @@ export default class HopSystem extends System {
 				return
 			} else if (frame === 0 || frame > hop.frame) {
 				hop.frame = frame
-				let sprite = entity.getMutableComponent!(Sprite)
+				let sprite = entity.getMutableComponent(Sprite)!
 				sprite.texture = this.animations[`hop-${hop.direction}`][
 					hop.frame
 				].textureCacheIds[0]
