f718d5df8055636baf15b36a14e74dcc3cbc4d09
Devin Spikowski <vegeta897@gmail.com>
2020-08-22 06:23:16 -0400

    Implement basic zoom on wheel event

--- packages/webapp/source/components/Game/Renderer/Renderer.ts
+++ packages/webapp/source/components/Game/Renderer/Renderer.ts
@@ -20,36 +20,38 @@ export default class Renderer {
 			screenHeight: this.app.view.offsetHeight,
 			interaction: this.app.renderer.plugins.interaction,
 		})
-		this.view
-			.drag()
-			.pinch() /*.wheel({ smooth: 10 })*/
-			.decelerate({ friction: 0.8 })
+		this.view.drag({ wheel: false }).pinch().decelerate({ friction: 0.8 })
 		this.view.moveCenter(0, 0)
 		this.view.scaled = 2
 		this.view.on('zoomed-end', () => {
 			console.log(this.view.scaled)
-			// if (this.view.scaled < 1) return
-			// if (this.view.scaled < 4) {
-			// 	this.view.animate({ time: 100, scale: Math.round(this.view.scaled) })
-			// }
 		})
-		canvas.addEventListener('mousewheel', (event) => {
-			let newZoom = 0
-			if (event.deltaY < 0) {
-				if (this.view.scaled <= 1) newZoom = this.view.scaled * 2
-				else newZoom = Math.round(this.view.scaled) + 1
-			} else if (event.deltaY > 0) {
-				if (this.view.scaled <= 1)
-					newZoom = Math.max(0.13, this.view.scaled / 2)
-				else newZoom = Math.round(this.view.scaled) - 1
-			}
-			if (newZoom === 0) console.log(event.deltaY, this.view.scaled)
-			if (newZoom !== this.view.scaled)
-				this.view.animate({
-					time: 100,
-					scale: newZoom,
-				})
-		})
+		canvas.addEventListener(
+			'wheel',
+			(event) => {
+				// Copied mostly from pixi-viewport wheel plugin
+				let point = this.app.renderer.plugins.interaction.mouse.global
+				let oldPoint = this.view.toLocal(point)
+				let viewChanged = false
+				if (event.deltaY < 0) {
+					this.view.scaled++
+					viewChanged = true
+				} else if (event.deltaY > 0) {
+					if (this.view.scaled > 1) {
+						this.view.scaled--
+						viewChanged = true
+					}
+				}
+				let newPoint = this.view.toGlobal(oldPoint)
+				if (viewChanged) {
+					this.view.x += point.x - newPoint.x
+					this.view.y += point.y - newPoint.y
+				}
+				event.preventDefault()
+				return false
+			},
+			false
+		)
 		this.view.sortableChildren = true
 		this.app.stage.addChild(this.view)
 		this.cull = new Cull.SpatialHash()
