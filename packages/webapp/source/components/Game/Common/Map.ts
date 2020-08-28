55674d19042ba1dc879d718035eb163dfbe471a8
Devin Spikowski <vegeta897@gmail.com>
2020-08-27 22:34:44 -0400

    Create Cell3D class to occupy map grids

--- packages/webapp/source/components/Game/Common/Map.ts
+++ packages/webapp/source/components/Game/Common/Map.ts
@@ -1,15 +1,48 @@
+import { Entity } from 'ecsy'
+
 export class Map3D {
-	private data: Map<string, any>
+	private data: Map<string, Cell3D>
 	constructor() {
 		this.data = new Map()
 	}
-	getXYZ(x: number, y: number, z: number) {
+	getXYZ(x: number, y: number, z: number): Cell3D | undefined {
 		return this.data.get(Map3D.xyzToHash(x, y, z))
 	}
-	setXYZ(x: number, y: number, z: number, value: any): void {
-		this.data.set(Map3D.xyzToHash(x, y, z), value)
+	addCell(cell: Cell3D): void {
+		// Throw if occupied?
+		this.data.set(cell.getHash(), cell)
+	}
+	clearXYZ(x: number, y: number, z: number): void {
+		this.data.delete(Map3D.xyzToHash(x, y, z))
 	}
 	private static xyzToHash(x: number, y: number, z: number): string {
 		return x + ':' + y + ':' + z
+	}
+}
+export class Cell3D {
+	private map: Map3D
+	private x: number
+	private y: number
+	private z: number
+	entity: Entity
+	constructor(map: Map3D, x: number, y: number, z: number, entity: Entity) {
+		this.map = map
+		this.x = x
+		this.y = y
+		this.z = z
+		this.entity = entity
+	}
+	getHash(): string {
+		return this.x + ':' + this.y + ':' + this.z
+	}
+	moveTo(x: number, y: number, z: number): void {
+		this.map.clearXYZ(this.x, this.y, this.z)
+		this.x = x
+		this.y = y
+		this.z = z
+		this.map.addCell(this)
+	}
+	destroy(): void {
+		this.map.clearXYZ(this.x, this.y, this.z)
 	}
 }
