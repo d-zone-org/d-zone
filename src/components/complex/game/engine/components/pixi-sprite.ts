b2dbf222a32da99edd1eda221da1242d36252eb8
Sagnik Pradhan <markten6321@gmail.com>
2020-11-25 16:09:31 +0530

    ♻️ Removed node-modules linker
    Latest version of next.js doesn't work under pnp.
    Look here https://github.com/vercel/next.js/issues/19334
    Fixed by downgrading next.
    
    Also added pixi.js as an alias to pixi.js-legacy
    As pixi-viewport was complaining under pnp

--- src/components/complex/game/engine/components/pixi-sprite.ts
+++ src/components/complex/game/engine/components/pixi-sprite.ts
@@ -1,10 +1,10 @@
-import { Component, Types } from 'ecsy'
-import * as PIXI from 'pixi.js-legacy'
+import { Component, Types } from "ecsy"
+import * as PIXI from "pixi.js-legacy"
 
 export default class PixiSprite extends Component<PixiSprite> {
-	value!: PIXI.Sprite
+  value!: PIXI.Sprite
 }
 
 PixiSprite.schema = {
-	value: { type: Types.Ref },
+  value: { type: Types.Ref },
 }
