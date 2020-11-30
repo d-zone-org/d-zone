3338013957e9c94a40eb016d361c93c2e092e277
Devin Spikowski <vegeta897@gmail.com>
2020-11-30 03:45:57 -0500

    :alien: Update hop system for ape-ecs

--- src/components/complex/game/engine/systems/hop.ts
+++ src/components/complex/game/engine/systems/hop.ts
@@ -1,89 +1,100 @@
-import { Attributes, System } from "ecsy"
-import Hop from "../components/hop"
-import Transform from "../components/transform"
-import Sprite from "../components/sprite"
-import MapCell from "../components/map-cell"
-import ActorCell from "../../common/actor-cell"
+import { Query, System } from 'ape-ecs'
+import Hop from '../components/hop'
+import Transform from '../components/transform'
+import Sprite from '../components/sprite'
+import MapCell from '../components/map-cell'
 import {
-  hopUpYOffsets,
-  hopDownYOffsets,
-  hopZDepthOffsets,
-} from "web/art/sprite-config.json"
+	hopUpYOffsets,
+	hopDownYOffsets,
+	hopZDepthOffsets,
+} from 'web/art/sprite-config.json'
 
 let hopFrameCount: number
 let hopFrameRate = 2
 
 export default class HopSystem extends System {
-  private animations: any
-  init(attributes: Attributes) {
-    this.animations = attributes.resources.sheet.spritesheet.animations
-    hopFrameCount = this.animations["hop-east"].length
-  }
-  execute(_delta: number, _time: number) {
-    let added = this.queries.hopping.added
-    if (added) {
-      added.forEach((entity) => {
-        let hop = entity.getMutableComponent(Hop)!
-        let actorCell = entity.getComponent(MapCell)!.value as ActorCell
-        let target = actorCell.getHopTarget(hop)
-        if (target) {
-          actorCell.reserveTarget(target)
-          actorCell.properties.platform = false
-          Object.assign(hop, target)
-        } else {
-          faceSpriteToHop(entity.getMutableComponent(Sprite)!, hop)
-          entity.removeComponent(Hop)
-        }
-      })
-    }
+	private animations: any
+	private hopQuery!: Query
+	init(resources: any) {
+		this.animations = resources.sheet.spritesheet.animations
+		hopFrameCount = this.animations['hop-east'].length
+		this.hopQuery = this.createQuery()
+			.fromAll(Hop, Transform, Sprite, MapCell)
+			.persist(true)
+	}
+	update(_tick: number) {
+		this.hopQuery.added.forEach((entity) => {
+			let hop = entity.c.hop as Hop
+			let actorCell = entity.c.mapCell.cell
+			let target = actorCell.getHopTarget(hop)
+			if (target) {
+				actorCell.reserveTarget(target)
+				actorCell.properties.platform = false
+				Object.assign(hop, target)
+			} else {
+				faceSpriteToHop(entity.c.sprite as Sprite, hop)
+				entity.removeComponent(typeof Hop)
+			}
+		})
 
-    let hopping = this.queries.hopping.results
-    for (let i = hopping.length - 1; i >= 0; i--) {
-      let entity = hopping[i]
-      let hop = entity.getMutableComponent(Hop)!
-      let frame = Math.floor(hopFrameCount * hop.progress)
-      if (hop.progress >= 1) {
-        let transform = entity.getMutableComponent(Transform)!
-        transform.x += hop.x
-        transform.y += hop.y
-        transform.z += hop.z
-        faceSpriteToHop(entity.getMutableComponent(Sprite)!, hop)
-        entity.getComponent(MapCell)!.value.properties.platform = true
-        entity.removeComponent(Hop)
-      } else if (hop.progress === 0 || frame > hop.frame) {
-        hop.frame = frame
-        let sprite = entity.getMutableComponent(Sprite)!
-        sprite.texture = this.animations[`hop-${hop.direction}`][
-          hop.frame
-        ].textureCacheIds[0]
-        if (hop.progress === 0) sprite.zIndex += 0.01
-        let zDepthOffsetIndex = hopZDepthOffsets.frames.indexOf(hop.frame)
-        if (zDepthOffsetIndex >= 0) {
-          // Adjust z-depth while hopping
-          sprite.zIndex +=
-            hopZDepthOffsets[hop.direction as keyof typeof hopZDepthOffsets][
-              zDepthOffsetIndex
-            ]
-        }
-        if (hop.z !== 0) {
-          // Raise or lower sprite while hopping up/down
-          let yOffsets = hop.z > 0 ? hopUpYOffsets : hopDownYOffsets
-          let yOffsetIndex = yOffsets.frames.indexOf(hop.frame)
-          if (yOffsetIndex >= 0) sprite.y += yOffsets.values[yOffsetIndex]
-        }
-      }
-      hop.progress += 1 / hopFrameCount / hopFrameRate
-    }
-  }
-}
-HopSystem.queries = {
-  hopping: {
-    components: [Hop, Transform, Sprite, MapCell],
-    listen: { added: true },
-  },
+		this.hopQuery.execute().forEach((entity) => {
+			let hop = entity.c.hop as Hop
+			let frame = Math.floor(hopFrameCount * hop.progress)
+			if (hop.progress >= 1) {
+				const { x, y, z } = entity.c.transform
+				entity.c.transform.update({
+					x: x + hop.x,
+					y: y + hop.y,
+					z: z + hop.z,
+				})
+				faceSpriteToHop(entity.c.sprite as Sprite, hop)
+				entity.c.mapCell.cell.properties.platform = true
+				entity.removeComponent(typeof Hop)
+			} else if (hop.progress === 0 || frame > hop.frame) {
+				hop.frame = frame
+				const sprite = entity.c.sprite
+				sprite.update({
+					texture: this.animations[`hop-${hop.direction}`][hop.frame]
+						.textureCacheIds[0],
+				})
+				if (hop.progress === 0) {
+					sprite.update({
+						zIndex: sprite.zIndex + 0.01,
+					})
+				}
+				let zDepthOffsetIndex = hopZDepthOffsets.frames.indexOf(hop.frame)
+				if (zDepthOffsetIndex >= 0) {
+					// Adjust z-depth while hopping
+					sprite.update({
+						zIndex:
+							sprite.zIndex +
+							hopZDepthOffsets[hop.direction as direction][zDepthOffsetIndex],
+					})
+				}
+				if (hop.z !== 0) {
+					// Raise or lower sprite while hopping up/down
+					let yOffsets = hop.z > 0 ? hopUpYOffsets : hopDownYOffsets
+					let yOffsetIndex = yOffsets.frames.indexOf(hop.frame)
+					if (yOffsetIndex >= 0) {
+						sprite.update({
+							y: sprite.y + yOffsets.values[yOffsetIndex],
+						})
+					}
+				}
+			}
+			hop.progress += 1 / hopFrameCount / hopFrameRate
+		})
+	}
 }
 function faceSpriteToHop(sprite: Sprite, hop: Hop) {
-  if (hop.direction === "east") sprite.texture = "cube:0"
-  else if (hop.direction === "south") sprite.texture = "cube:1"
-  else sprite.texture = "cube:2"
+	sprite.update({
+		texture: hopDirectionTextures[hop.direction as direction],
+	})
+}
+type direction = 'east' | 'west' | 'south' | 'north'
+const hopDirectionTextures = {
+	east: 'cube:0',
+	south: 'cube:1',
+	west: 'cube:2',
+	north: 'cube:2',
 }
