af1c36a440b8da627e6b87d308f3da2c3763b685
Devin Spikowski <vegeta897@gmail.com>
2020-12-27 01:09:36 -0500

    :art: Undo bad prettier formatting

--- packages/pixi-viewport/src/viewport.es.js
+++ packages/pixi-viewport/src/viewport.es.js
@@ -1636,26 +1636,26 @@ var penner = createCommonjsModule(function (module, exports) {
 	Copyright © 2001 Robert Penner
 	All rights reserved.
 
-	Redistribution and use in source and binary forms, with or without modification, 
+	Redistribution and use in source and binary forms, with or without modification,
 	are permitted provided that the following conditions are met:
 
-	Redistributions of source code must retain the above copyright notice, this list of 
+	Redistributions of source code must retain the above copyright notice, this list of
 	conditions and the following disclaimer.
-	Redistributions in binary form must reproduce the above copyright notice, this list 
-	of conditions and the following disclaimer in the documentation and/or other materials 
+	Redistributions in binary form must reproduce the above copyright notice, this list
+	of conditions and the following disclaimer in the documentation and/or other materials
 	provided with the distribution.
 
-	Neither the name of the author nor the names of contributors may be used to endorse 
+	Neither the name of the author nor the names of contributors may be used to endorse
 	or promote products derived from this software without specific prior written permission.
 
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 	MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
 	COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
 	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-	GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+	GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
 	AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
 	OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
@@ -2246,15 +2246,8 @@ class Snap extends Plugin {
 	 * @param {number} y
 	 * @param {SnapOptions} [options]
 	 * @event snap-start(Viewport) emitted each time a snap animation starts
-	 * @event snap-restart(Viewport) emitted each time a snap resets because of
-	 *
-	 *
-	 *
-	 *
-	 *           a
-	 *
-	 *
-	 *          change in viewport size
+	 * @event snap-restart(Viewport) emitted each time a snap resets because of a
+	 *     change in viewport size
 	 * @event snap-end(Viewport) emitted each time snap reaches its target
 	 * @event snap-remove(Viewport) emitted if snap plugin is removed
 	 */
