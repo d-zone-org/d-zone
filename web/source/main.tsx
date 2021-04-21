1b60a1de9cb58951c4909de4263209bf114e1f80
Sagnik Pradhan <markten6321@gmail.com>
2021-04-21 09:13:54 +0530

    🔥 Removed next.js specific code
    Enhanced eslint configuration
    Fixed prettier pnp issue by making a JS config

---
+++ web/source/main.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <h1>Hey there</h1>
+
+ReactDOM.render(<App />, document.getElementById('root'))
--- web/src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { AppProps } from 'next/app'
-
-// eslint-disable-next-line jsdoc/require-jsdoc
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
-}
