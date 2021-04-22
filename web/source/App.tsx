823d42261d8d4fa8159ebf9ca2a6b14ec6f9b5b3
Sagnik Pradhan <markten6321@gmail.com>
2021-04-22 13:31:51 +0530

    ✨ Added tailwindcss

--- web/source/App.tsx
+++ web/source/App.tsx
@@ -3,7 +3,9 @@ import Game from './components/game'
 
 export const App = () => {
 	return (
-		<div>
+		<div className="flex flex-col items-center">
+			<h1 className="m-2 font-bold text-4xl">D Zone</h1>
+
 			<Game />
 		</div>
 	)
