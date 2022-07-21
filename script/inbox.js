4640a33d58ec5fde65966ddbc6e93fae56532ac4
Devin Spikowski <vegeta897@gmail.com>
2022-07-21 16:52:06 -0400

    Fix ignoreUsers and exclude from user list, fixes #277

--- script/inbox.js
+++ script/inbox.js
@@ -73,7 +73,7 @@ function Inbox(config) {
             if(config.get('infoCommand') && config.get('url') && message === config.get('infoCommand')) return respond(channel);
             if(server.hideOffline && (!member.status || member.status === 'offline')) return;
             if(server.ignoreUsers && // Check if this user is ignored
-                server.ignoreUsers.indexOf(author.id)) return;
+                server.ignoreUsers.indexOf(author.id) >= 0) return;
             if(server.ignoreChannels && // Check if this channel is ignored
                 (server.ignoreChannels.indexOf(channel.name) >= 0 ||
                     server.ignoreChannels.indexOf(channel.id) >= 0)) return;
@@ -118,6 +118,7 @@ Inbox.prototype.getUsers = function(connectRequest) {
     let users = {};
     for(let [uid, member] of guild.members) {
         if(server.hideOffline && (!member.status || member.status === 'offline')) continue;
+        if(server.ignoreUsers && server.ignoreUsers.indexOf(uid) >= 0) continue;
         users[uid] = {
             uid,
             username: member.nick || member.username,
